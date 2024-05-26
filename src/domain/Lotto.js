a357be6739c64fd72d5ab7fa8f3360bc0e684fc3
supreme-d <dmhuh.data@gmail.com>
2024-05-27 01:25:48 +0900

    feat: 당첨 결과 확인 메소드 추가

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -12,4 +12,17 @@ export default class Lotto {
   #sortNumbers(numbers) {
     return [...numbers].sort((a, b) => a - b);
   }
+
+  getResult(winningNumbers, bonusNumber) {
+    let matchedCount = 0;
+    const thisNumbersSet = new Set(this.#numbers);
+
+    winningNumbers.forEach((winningNumber) => {
+      if (thisNumbersSet.has(winningNumber)) {
+        matchedCount++;
+      }
+    });
+
+    return { matchedCount, hasBonusNumber: thisNumbersSet.has(bonusNumber) };
+  }
 }
