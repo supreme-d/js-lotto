3fff67cb39215bb703b6b76892f0e53cef06eb7e
supreme-d <dmhuh.data@gmail.com>
2024-05-27 01:22:27 +0900

    test: 로또 당첨 결과 확인 테스트 추가

--- __tests__/domain/Lotto.test.js
+++ __tests__/domain/Lotto.test.js
@@ -10,4 +10,17 @@ describe("로또", () => {
       expect(lotto.getNumbers()).toStrictEqual(orderedNumbers);
     });
   });
+
+  describe("당첨 결과 확인", () => {
+    it("당첨 번호와 일치하는 로또 번호의 개수와 보너스 번호 포함 여부를 반환해야 한다", () => {
+      const winningNumbers = [1, 2, 3, 4, 5, 6];
+      const bonusNumber = 7;
+
+      const lotto = new Lotto([1, 2, 8, 9, 10, 11]);
+      const result = lotto.getResult(winningNumbers, bonusNumber);
+
+      expect(result.matchedCount).toBe(2);
+      expect(result.hasBonusNumber).toBeFalsy();
+    });
+  });
 });
